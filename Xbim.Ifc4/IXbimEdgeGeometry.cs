7b071dc2fc9b0bf3717d82362b4f5ce33e31530e
ncr-399 <veer.yagnik@ncircletech.com>
2024-03-07 14:27:30 +0530

    added IXbimEdgeGeometry interface

---
+++ Xbim.Ifc4/IXbimEdgeGeometry.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xbim.Common.Geometry;
+
+using Xbim.Ifc4.Interfaces;
+
+namespace Xbim.Ifc4
+{
+    public interface IXbimEdgeGeometry
+    {
+        IXbimEdgeSet CreateEdge(IIfcBoundingBox ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcSweptDiskSolid ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcShellBasedSurfaceModel ifcSurface, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcSurfaceCurveSweptAreaSolid ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcBoxedHalfSpace ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcPolygonalBoundedHalfSpace ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcHalfSpaceSolid ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcFaceBasedSurfaceModel ifcSurface, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcTriangulatedFaceSet shell, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcSweptAreaSolid ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcExtrudedAreaSolid ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcCsgPrimitive3D ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcSphere ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcBlock ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcRightCircularCylinder ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcRevolvedAreaSolid ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcRectangularPyramid ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcRightCircularCone ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcRevolvedAreaSolidTapered ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcFixedReferenceSweptAreaSolid ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcAdvancedBrep ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcAdvancedBrepWithVoids ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcSectionedSpine ifcSolid, ILogger logger);
+        IXbimEdgeSet CreateEdge(IIfcSweptDiskSolidPolygonal ifcSolid, ILogger logger);
+    }
+}
